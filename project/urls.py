628964bf173ee3fe60e13a27ac9d145821cf7c12
Pablo Alves <PabloHAlves99@gmail.com>
2024-01-18 15:03:11 -0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def my_view(request):
+    print('Deu bom')
+    return HttpResponse('Deu bom no site')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', my_view),
 ]
